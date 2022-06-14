6f3557ced671a004c0fb74b345150d922836d104
risako070310 <risako070310@gmail.com>
2022-06-15 04:54:54 +0900

    add table content

--- TechPod/ViewController.swift
+++ TechPod/ViewController.swift
@@ -7,26 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet var table: UITableView!
+    
+    var songNameArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         table.dataSource = self
+        
+        table.delegate = self
+        
+        songNameArray = ["カノン", "エリーゼのために", "G線上のアリア"]
+    }
+    
+    override func didReceiveMemoryWarning(){
+        super.didReceiveMemoryWarning()
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return songNameArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
-        cell?.textLabel?.text = "テスト"
+        
+        cell?.textLabel?.text = songNameArray[indexPath.row]
         
         return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(songNameArray[indexPath.row])が選ばれました！")
     }
 }
 
